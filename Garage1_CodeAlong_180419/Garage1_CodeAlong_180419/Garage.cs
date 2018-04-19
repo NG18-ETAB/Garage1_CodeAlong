5fa30f2ba18928c68982264d0c0cb4dd7f318219
John Hellman <acoppero@gmail.com>
2018-04-19 12:00:17 +0200

    Lunch Break

---
+++ Garage1_CodeAlong_180419/Garage1_CodeAlong_180419/Garage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Garage1_CodeAlong_180419.Vehicles;
+
+namespace Garage1_CodeAlong_180419
+{
+    public class Garage<T> : IEnumerable<T> where T : Vehicle
+    {
+        private T[] internalCollection;
+        private int _count, _capacity;
+
+        public Garage(int capacity, out string message)
+        {
+            message = "The Garage has been set to size " + capacity;
+
+            if (capacity < 1)
+            {
+                capacity = 1;
+                message = "The Garage has to be at least size 1"+
+                    "\nThe Garage has been set to size 1";
+            }
+
+            internalCollection = new T[capacity];
+
+            _capacity = capacity;
+            _count = 0;
+        }
+
+        public void Park(T vehicle, out string message)
+        {
+            message = "Sadly the garage is currently full";
+            if (_count < _capacity)
+            {
+                message = "The vehicle has been parked";
+                internalCollection[_count++] = vehicle;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Garage1_CodeAlong_180419/Garage1_CodeAlong_180419/Vehicles/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage1_CodeAlong_180419.Vehicles
+{
+    public class Car : Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public Car(string regNr, uint nrOfWheels, string color,
+            string fuelType, string make, string model) : 
+            base(regNr, nrOfWheels, color, fuelType)
+        {
+            Make = make;
+            Model = model;
+        }
+    }
+}
